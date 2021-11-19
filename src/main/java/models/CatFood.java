f3ab89ef9dbe9264ec18ec87b3bf0532978929ed
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-19 18:07:50 -0500

    catfood accessors and mutators

--- src/main/java/models/CatFood.java
+++ src/main/java/models/CatFood.java
@@ -6,5 +6,53 @@ public class CatFood {
     private String type;
     private int weight;
     private int qty;
-    private int price;
+    private double price;
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public Integer getQty() {
+        return this.qty;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
 }
--- src/test/java/models/CatFoodTest.java
+++ src/test/java/models/CatFoodTest.java
@@ -1,4 +1,86 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
 public class CatFoodTest {
+    private CatFood testCatFood = new CatFood();
+
+    @Test
+    public void setBrandTest() {
+        //Given
+        String expected = "Fancy Feast";
+
+        //When
+        testCatFood.setBrand(expected);
+        String actual = testCatFood.getBrand();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setTypeTest() {
+        //Given
+        String expected = "dry";
+
+        //When
+        testCatFood.setType(expected);
+        String actual = testCatFood.getType();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setWeightTest() {
+        //Given
+        Integer expected = 5;
+
+        //When
+        testCatFood.setWeight(expected);
+        Integer actual = testCatFood.getWeight();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setQtyTest() {
+        //Given
+        Integer expected = 100;
+
+        //When
+        testCatFood.setQty(expected);
+        Integer actual = testCatFood.getQty();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setPriceTest() {
+        //Given
+        Double expected = 3.50;
+
+        //When
+        testCatFood.setPrice(expected);
+        Double actual = testCatFood.getPrice();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setIdTest() {
+        //Given
+        Integer expected = 12345;
+
+        //When
+        testCatFood.setId(expected);
+        Integer actual = testCatFood.getId();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
