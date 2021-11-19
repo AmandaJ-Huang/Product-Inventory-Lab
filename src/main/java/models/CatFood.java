461f06cb97870a75cf2777ab6e38567c326185c8
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-19 16:12:31 -0500

    added properties to CatFood model

--- src/main/java/models/CatFood.java
+++ src/main/java/models/CatFood.java
@@ -1,4 +1,10 @@
 package models;
 
 public class CatFood {
+    private int id;
+    private String brand;
+    private String type;
+    private int weight;
+    private int qty;
+    private int price;
 }
