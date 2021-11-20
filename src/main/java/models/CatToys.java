87d6490b3db7f1cb6f6256895a731bc7bbac3cb2
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-19 19:54:54 -0500

    CatToys accessors, mutators, and constructors

--- src/main/java/models/CatToys.java
+++ src/main/java/models/CatToys.java
@@ -1,4 +1,51 @@
 package models;
 
 public class CatToys {
+    private int id;
+    private String type;
+    private Character size;
+    private double price;
+
+    public CatToys() {
+
+    }
+
+    public CatToys(Integer id, String type, Character size, Double price) {
+        this.id = id;
+        this.type = type;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setSize(Character size) {
+        this.size = size;
+    }
+
+    public Character getSize() {
+        return this.size;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
 }
--- src/test/java/models/CatToysTest.java
+++ src/test/java/models/CatToysTest.java
@@ -1,4 +1,82 @@
 package models;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class CatToysTest {
+    CatToys testCatToys = new CatToys();
+
+    @Test
+    public void setIdTest() {
+        //Given
+        Integer expected = 161199;
+
+        //When
+        testCatToys.setId(expected);
+        Integer actual = testCatToys.getId();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setTypeTest() {
+        //Given
+        String expected = "teaser";
+
+        //When
+        testCatToys.setType(expected);
+        String actual = testCatToys.getType();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setSizeTest() {
+        //Given
+        Character expected = 'S';
+
+        //When
+        testCatToys.setSize(expected);
+        Character actual = testCatToys.getSize();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setPriceTest() {
+        //Given
+        Double expected = 4.99;
+
+        //When
+        testCatToys.setPrice(expected);
+        Double actual = testCatToys.getPrice();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void constructorTest() {
+        //Given
+        Integer expectedId = 161807;
+        String expectedType = "springs";
+        Character expectedSize = 'M';
+        Double expectedPrice = 3.48;
+
+        //When
+        CatToys friscoSprings = new CatToys(expectedId, expectedType, expectedSize, expectedPrice);
+        Integer actualId = friscoSprings.getId();
+        String actualType = friscoSprings.getType();
+        Character actualSize = friscoSprings.getSize();
+        Double actualPrice = friscoSprings.getPrice();
+
+        //Then
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedType, actualType);
+        Assert.assertEquals(expectedSize, actualSize);
+        Assert.assertEquals(expectedPrice, actualPrice);
+    }
 }
